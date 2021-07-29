b1a3becae0d2b6ba65540e9eff56f88d193c04c9
Oren Zipori <orenzp@hotmail.com>
2021-07-29 15:25:11 +0300

    Adding message function for YADM bootstrap script

--- .config/yadm/bootstrap.d/ubuntu.sh
+++ .config/yadm/bootstrap.d/ubuntu.sh
@@ -1,13 +1,26 @@
 #!/bin/bash
-echo "Adding PPA repositorys"
-curl -fsSL https://apt.releases.hashicorp.com/gpg | sudo apt-key add - \
-    && sudo apt-add-repository "deb [arch=amd64] https://apt.releases.hashicorp.com $(lsb_release -cs) main" 
+# Function for echo format
+function message () {
+  echo -e "\e[1;32m
+  ##################
+  ##
+  ##  $1
+  ##
+  ##################\e[0m
+  "
+}
+
+message "Adding PPA repositorys"
+sleep 5
+curl -fsSL https://apt.releases.hashicorp.com/gpg | sudo apt-key add -
+sudo apt-add-repository "deb [arch=amd64] https://apt.releases.hashicorp.com $(lsb_release -cs) main" 
 sudo apt-add-repository ppa:fish-shell/release-3
 sudo add-apt-repository ppa:git-core/ppa # stable PPA for latest stable Git version
-sudo curl -fsSLo /usr/share/keyrings/kubernetes-archive-keyring.gpg https://packages.cloud.google.com/apt/doc/apt-key.gpg \
-    && sudo echo "deb [signed-by=/usr/share/keyrings/kubernetes-archive-keyring.gpg] https://apt.kubernetes.io/ kubernetes-xenial main" | sudo tee /etc/apt/sources.list.d/kubernetes.list
+sudo curl -fsSLo /usr/share/keyrings/kubernetes-archive-keyring.gpg https://packages.cloud.google.com/apt/doc/apt-key.gpg
+sudo echo "deb [signed-by=/usr/share/keyrings/kubernetes-archive-keyring.gpg] https://apt.kubernetes.io/ kubernetes-xenial main" | sudo tee /etc/apt/sources.list.d/kubernetes.list
 
-echo "Install desired packages "
+message "Install Ubuntu packages"
+sleep 5
 sudo apt-get update && apt upgrade && export DEBIAN_FRONTEND=noninteractive 
 sudo apt-get -y install --no-install-recommends bash-completion vim \
     software-properties-common dirmngr apt-transport-https lsb-release \
@@ -17,17 +30,17 @@ sudo apt-get -y install --no-install-recommends bash-completion vim \
 read -p "Do you want to install Azure CLI?? (y/n)?" choice
 case "$choice" in 
   y|Y ) curl -sL https://aka.ms/InstallAzureCLIDeb | sudo bash ;;
-  n|N ) echo "Aboring Azure CLI installation";;
-  * ) echo "invalid answer";;
+  n|N ) message "Aboring Azure CLI installation";;
+  * ) message "invalid answer";;
 esac
 
-echo "Setup Kubectl, HELM and add-ons"
+message "Setup Kubectl, HELM and add-ons"
+sleep 5
 if ! command -v helm &> /dev/null
 then
-    echo "Installing HELM"
     curl https://raw.githubusercontent.com/helm/helm/master/scripts/get-helm-3 | bash
 else
-    echo "HELM Found $(helm version --short)"
+    message "HELM Found $(helm version --short)"
 fi
 
 curl -L https://github.com/wercker/stern/releases/download/1.11.0/stern_linux_amd64 \
@@ -37,12 +50,12 @@ curl -s "https://raw.githubusercontent.com/\
 kubernetes-sigs/kustomize/master/hack/install_kustomize.sh"  | bash \
     && sudo kustomize /usr/local/bin
 
-echo "Runnin small commands"
+message "Runnin small commands" "\e[1;32m$1\e[0m
 terraform -install-autocomplete
 
-read -p "Do you want to decrypt YADM Files? (y/n)?" choice
+read -p "\e[1;32mDo you want to decrypt YADM Files? (y/n)?\e[0m choice
 case "$choice" in 
   y|Y ) yadm decrypt ;;
-  n|N ) echo "Aborting YADM Decrypt";;
-  * ) echo "invalid answer";;
+  n|N ) message "Aborting YADM Decrypt";;
+  * ) message "invalid answer";;
 esac
