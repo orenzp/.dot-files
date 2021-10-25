8ddb20ce2694006326e93e420ce707bc5a153a0b
Oren Zipori <orenzi@etoro.com>
2021-10-25 10:37:21 +0300

    Create a bootstartp template for macOS for brew setup

--- .config/yadm/bootstrap.d/update_repo_URL.sh
+++ .config/yadm/bootstrap.d/update_repo_URL.sh
@@ -1,4 +1,3 @@
 #!/bin/sh
-
 echo "Updating the yadm repo origin URL to SSH form HTTPS"
-yadm remote set-url origin "git@github.com:orenzp/.dot-files.git"
+yadm remote set-url origin git@github-privet:orenzp/.dot-files.git
